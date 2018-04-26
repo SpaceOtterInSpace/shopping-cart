090886f45841d0578bf55b6e14bb1c3a80180964
Jacqui Nelson <jnelson@cotingasoft.com>
2018-04-26 17:51:05 -0600

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CartHeader from './CartHeader.js'
+import CartFooter from './CartFooter.js'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <CartHeader />
+          <CartFooter />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
