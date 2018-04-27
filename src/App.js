db44899c5ded688b203fc8ed664786310fb92329
Jacqui Nelson <nelson@visionlink.org>
2018-04-27 09:00:57 -0600

    Add Cart Items section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CartHeader from './CartHeader.js'
+import CartItems from './components/CartItems.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <CartHeader />
+        <CartHeader />
+        <CartItems />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
