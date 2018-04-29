3b43633271d261d23fb39bb720c48385c7182c50
Jacqui Nelson <jnelson@cotingasoft.com>
2018-04-29 15:14:57 -0600

    Add cart items

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import CartFooter from './components/CartFooter.js'
 
 class App extends Component {
   render() {
+    let cartItemsList = [
+      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+    ];
     return (
       <div className="App">
         <header className="App-header">
           <CartHeader />
-          <CartItems />
+          <CartItems cartItemsList={cartItemsList}/>
           <CartFooter copyright="2018" />
         </header>
       </div>
--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-function CartItems() {
+function CartItems({cartItemsList}) {
   return (
     <div className="container">
       <h1>Cart Items</h1>
+      <div className="list-group">
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-md-8">Product</div>
+            <div className="col-md-2">Price</div>
+            <div className="col-md-2">Quantity</div>
+          </div>
+        </div>
+        {cartItemsList.map(cartItem => <CartItem key={ cartItem.id } cartItem={ cartItem } />)}
+      </div>
+    </div>
+  )
+}
+
+function CartItem({cartItem}) {
+  return (
+    <div className="list-group-item">
+      <div class="collection-item">
+        <div class="row">
+          <div class="col-md-8">{cartItem.product.name}</div>
+          <div class="col-md-2">${cartItem.product.priceInCents/100}</div>
+          <div class="col-md-2">{cartItem.quantity}</div>
+        </div>
+      </div>
     </div>
   )
 }
