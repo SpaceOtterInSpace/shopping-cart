48d10de96dd285a6ca156864fd34d9316fad5fff
Jacqui Nelson <jnelson@cotingasoft.com>
2018-04-29 19:06:20 -0600

    Fix class to be className

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -21,11 +21,11 @@ function CartItems({cartItemsList}) {
 function CartItem({cartItem}) {
   return (
     <div className="list-group-item">
-      <div class="collection-item">
-        <div class="row">
-          <div class="col-md-8">{cartItem.product.name}</div>
-          <div class="col-md-2">${cartItem.product.priceInCents/100}</div>
-          <div class="col-md-2">{cartItem.quantity}</div>
+      <div className="collection-item">
+        <div className="row">
+          <div className="col-md-8">{cartItem.product.name}</div>
+          <div className="col-md-2">${cartItem.product.priceInCents/100}</div>
+          <div className="col-md-2">{cartItem.quantity}</div>
         </div>
       </div>
     </div>
